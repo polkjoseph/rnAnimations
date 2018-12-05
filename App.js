ef961cad7cc23658468394a4325de8b69185139a
polkjoseph <jpolk@xonicsystems.com>
2018-12-05 01:47:37 -0800

    added Ball component (learning animations)

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Ball from './src/Ball';
 
 export default class App extends React.Component {
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={ styles.container }>
+        <Text> Open up </Text>
+        <Ball />
       </View>
     );
   }
