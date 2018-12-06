5bb934ef831cb699e3112f57262d192b88d7a428
polkjoseph <jpolk@xonicsystems.com>
2018-12-05 22:31:30 -0800

    ball moving

--- App.js
+++ App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,   View } from 'react-native';
 import Ball from './src/Ball';
 
 export default class App extends React.Component {
   
   render() {
     return (
-      <View style={ styles.container }>
-        <Text> Open up </Text>
+      <View>
         <Ball />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
